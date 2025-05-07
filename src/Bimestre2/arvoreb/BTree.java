1be731c6793f138dad14af9bb081d003d6011e4f
Daniel dos Santos <danieldossantxs@gmail.com>
2025-05-06 22:19:20 -0300

    Implementando Exclusão
    Implementar:
        BTree - localizarNo
        BTree - localizarSubE
        BTree - localizarSubD
        No - remanejarExclusao
    Continuar implementação:
        BTree - resdistribui_concatena

--- src/Bimestre2/arvoreb/BTree.java
+++ src/Bimestre2/arvoreb/BTree.java
@@ -32,14 +32,12 @@ public class BTree {
         return pai;
     }
 
-    private void split(No folha, No pai)
-    {
+    private void split(No folha, No pai) {
         No cx1 = new No();
         No cx2 = new No();
         int pos;
 
-        for(int i=0; i<No.m; i++)
-        {
+        for(int i=0; i<No.m; i++) {
             cx1.setvInfo(i, folha.getvInfo(i));
             cx1.setvPos(i, folha.getvPos(i));
             cx1.setvLig(i, folha.getvLig(i));
@@ -48,8 +46,7 @@ public class BTree {
         cx1.setvLig(No.m, folha.getvLig(No.m));
         cx1.setTl(No.m);
 
-        for(int i=No.m+1; i<2*No.m+1; i++)
-        {
+        for(int i=No.m+1; i<2*No.m+1; i++) {
             cx2.setvInfo(i-(No.m+1), folha.getvInfo(i));
             cx2.setvPos(i-(No.m+1), folha.getvPos(i));
             cx2.setvLig(i-(No.m+1), folha.getvLig(i));
@@ -58,16 +55,14 @@ public class BTree {
         cx2.setvLig(No.m, folha.getvLig(2*No.m+1));
         cx2.setTl(No.m);
 
-        if(folha == pai)
-        {
+        if(folha == pai) {
             folha.setvInfo(0, folha.getvInfo(No.m));
             folha.setvPos(0, folha.getvPos(No.m));
             folha.setTl(1);
             folha.setvLig(0, cx1);
             folha.setvLig(1, cx2);
         }
-        else
-        {
+        else {
             pos = pai.procurarPosicao(folha.getvInfo(No.m));
             pai.remanejar(pos);
             pai.setvInfo(pos, folha.getvInfo(No.m));
@@ -84,43 +79,136 @@ public class BTree {
         }
     }
 
-    public void inserir(int info, int posArq)
-    {
+    public void inserir(int info, int posArq) {
         No folha, pai;
         int pos;
+
         if (raiz==null)
             raiz = new No(info, posArq);
-        else
-        {
+        else {
             folha = navegarAteFolha(info);
             pos = folha.procurarPosicao(info);
             folha.remanejar(pos);
             folha.setvInfo(pos, info);
             folha.setvPos(pos, posArq);
             folha.setTl(folha.getTl()+1);
-            if(folha.getTl()>2*No.m)
-            {
+
+            if(folha.getTl()>2*No.m) {
                 pai = localizarPai(folha, info);
                 split(folha, pai);
             }
         }
     }
 
-    public void in_ordem()
-    {
+    public void in_ordem() {
         in_ordem(raiz);
     }
 
-    private void in_ordem(No raiz)
-    {
-        if (raiz != null)
-        {
-            for(int i=0; i<raiz.getTl(); i++)
-            {
+    private void in_ordem(No raiz) {
+        if (raiz != null) {
+            for(int i=0; i<raiz.getTl(); i++) {
                 in_ordem(raiz.getvLig(i));
                 System.out.println(raiz.getvInfo(i));
             }
+
             in_ordem(raiz.getvLig(raiz.getTl()));
+        }
+    }
+
+    //Exclusão
+    public No localizarNo(int info) {
+        return null;
+    }
+
+    private No localizarSubE(No no, int pos) {
+        return null;
+    }
+
+    private No localizarSubD(No no, int pos) {
+        return null;
+    }
+
+    private void resdistribui_concatena(No folha) {
+        No pai = localizarPai(folha, folha.getvInfo(0));
+        int posPai = pai.procurarPosicao(folha.getvInfo(0));
+        No irmaE, irmaD;
+
+        if (posPai > 0)
+            irmaE = pai.getvLig(posPai - 1);
+        else
+            irmaE = null;
+
+        if (posPai < pai.getTl())
+            irmaD = pai.getvLig(posPai + 1);
+        else
+            irmaD = null;
+
+        //redistribuição com a irmã da esquerda
+        if (irmaE != null && irmaE.getTl() > No.m) {
+            folha.remanejar(0);
+            folha.setvInfo(0, pai.getvInfo(posPai - 1));
+            folha.setvPos(0, pai.getvPos(posPai - 1));
+            folha.setTl(folha.getTl() + 1);
+            pai.setvInfo(posPai - 1, irmaE.getvInfo(irmaE.getTl() - 1 ));
+            pai.setvPos(posPai - 1, irmaE.getvPos(irmaE.getTl() - 1 ));
+            folha.setvLig(0, irmaE.getvLig(irmaE.getTl()));
+            irmaE.setTl(irmaE.getTl() - 1);
+        }
+        else
+            //redistribuição com a irmã da direita
+            if (irmaD != null && irmaD.getTl() > No.m) {
+
+            }
+            else {
+                //concatenação com a irmã da esquerda
+                if (irmaE != null) {
+
+                }
+                //concatenação com a irmã da direita
+                else {
+
+                }
+            }
+    }
+
+    public void exclusao(int info) {
+        No no = localizarNo(info);
+        int pos = 0;
+        No subE, subD, folha = null;
+
+        if (no != null) {
+            pos = no.procurarPosicao(info);
+
+            if (no.getvLig(0) != null) { //não é folha
+                subE = localizarSubE(no, pos);
+                subD = localizarSubD(no, pos + 1);
+
+                if (subE.getTl() > No.m || subD.getTl() == No.m) {
+                    no.setvInfo(pos, subE.getvInfo(subE.getTl() - 1));
+                    no.setvPos(pos, subE.getvPos(subE.getTl() - 1));
+                    folha = subE;
+                    pos = subE.getTl() - 1;
+                }
+                else {
+                    no.setvInfo(pos, subD.getvInfo(0));
+                    no.setvPos(pos, subD.getvPos(0));
+                    folha = subD;
+                    pos = 0;
+                }
+            }
+            else
+                folha = no;
+
+            //Excluir da folha
+            folha.remanejarExclusao(pos);
+            folha.setTl(folha.getTl() - 1);
+
+            if (folha == raiz && raiz.getTl() == 0)
+                raiz = null;
+            else
+                if (folha != raiz && folha.getTl() < No.m)
+                    resdistribui_concatena(folha);
+
         }
     }
 }
--- src/Bimestre2/arvoreb/No.java
+++ src/Bimestre2/arvoreb/No.java
@@ -7,39 +7,40 @@ public class No {
     private No vLig[];
     private int tl;
 
-    public No()
-    {
+    public No() {
         vInfo = new int[2*m+1];
         vPos = new int[2*m+1];
         vLig = new No[2*m+2];
         tl = 0;
     }
 
-    public No(int info, int posArq)
-    {
+    public No(int info, int posArq) {
         this();
         vInfo[0] = info;
         vPos[0] = posArq;
         tl=1;
     }
 
-    public int procurarPosicao(int info)
-    {
+    public int procurarPosicao(int info) {
         int pos=0;
+
         while(pos<tl && info>vInfo[pos])
             pos++;
+
         return pos;
     }
 
-    public void remanejar(int pos)
-    {
+    public void remanejar(int pos) {
         vLig[tl+1] = vLig[tl];
-        for(int i=tl; i>pos; i--)
-        {
+
+        for(int i=tl; i>pos; i--) {
             vInfo[i] = vInfo[i-1];
             vPos[i] = vPos[i-1];
             vLig[i] = vLig[i-1];
         }
+    }
+
+    public void remanejarExclusao(int pos) {
     }
 
     public int getvInfo(int p) {
